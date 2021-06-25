769aec97f0d134933f39056d5f1feffe0e17fc91
Anthony Villarroel <60784810+Villatony99@users.noreply.github.com>
2021-06-24 20:43:16 -0500

    clase

--- Modulo 1/programming/01_Projects/index.js
+++ Modulo 1/programming/01_Projects/index.js
@@ -3,7 +3,24 @@ const express =require('express');
 const app = express()
 //EndPoint
 app.get('/', (req,res)=>{
-res.send("Hola Mundo con ExpressJs")
+res.send("Hola Mundo soy anthony")
+})
+
+app.get('/saludo',(req,res)=>{
+    req.query
+    const {
+        query: { nombre, apellido } } = req;
+
+    res.json({
+        saludo:`Hola tu eres ${nombre} ${apellido}` ,
+    })
+})
+
+app.get('/saludo/:nombre',(req,res)=>{
+    const { params : { nombre } } = req
+    res.json({
+        nombre:nombre
+    })
 })
 app.listen(3000,()=>{
     console.log("Servidor escuchando http://localhost:3000")
---
+++ Modulo 1/programming/01_Projects/routes/index.js
@@ -0,0 +1,15 @@
+//Traemos el paquete
+const { Router } = require('express')
+
+//Inicializamos Router
+const router = Router()
+
+//Creacion de los endPoint
+
+
+module.exports.RouterIndex = router
+// module = {
+//     exports:{
+//         RouterIndex:router
+//     }
+// }
