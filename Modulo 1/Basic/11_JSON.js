0fcb94400437b77ded8f5511e4b98b113740564c
Anthony Villarroel <60784810+Villatony99@users.noreply.github.com>
2021-06-26 21:49:05 -0500

    clase

--- Modulo 1/Basic/11_JSON.js
+++ Modulo 1/Basic/11_JSON.js
@@ -4,6 +4,6 @@ var modifiedData = data.map((person)=>{
 person.first_name=person.first_name.toLocaleUpperCase()
 return person
 })
-var filterData=modifiedData.filter((person)=>person.id===3)
-//console.log(modifiedData)
-console.log(filterData)
+var filterData=data.filter((person)=>person.first_name=="maiga")
+console.log(modifiedData)
+//console.log(filterData)
